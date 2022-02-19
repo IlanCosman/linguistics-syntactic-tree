e533361bcc7e238ac6b9585ad0454f3b235c1a83
Ilan Cosman <ilancosman@gmail.com>
2022-02-19 14:52:58 -0800

    Add basic formatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { Range, TextDocument, TextEdit } from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "lst" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("lst.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from lst!");
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider(
+      "lst",
+      formattingEditProvider
+    )
+  );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+const formattingEditProvider: vscode.DocumentFormattingEditProvider = {
+  provideDocumentFormattingEdits: async (document, _, token) => {
+    const edits = await getFormatRangeEdits(document);
+    return token.isCancellationRequested ? [] : (edits as TextEdit[]);
+  },
+};
+
+const getFormatRangeEdits = async (
+  document: TextDocument
+): Promise<ReadonlyArray<TextEdit>> => {
+  let result = document.getText();
+  result = result.replace(/\[/g, "\n[");
+  return [
+    new TextEdit(new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE), result),
+  ];
+};
