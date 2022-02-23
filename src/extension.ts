a6f58385afa3f22675e65e8215010ac020328970
Ilan Cosman <ilancosman@gmail.com>
2022-02-23 14:30:11 -0800

    Shorten code

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,13 @@
 import * as vscode from "vscode";
+import { CompletionItem, Position, Range, TextDocument } from "vscode";
 import * as rules from "./rules.json";
-import {
-  CompletionItem,
-  Position,
-  Range,
-  TextDocument,
-  TextEdit,
-} from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.languages.registerDocumentFormattingEditProvider(
       "lst",
       formattingEditProvider
-    )
-  );
-  context.subscriptions.push(
+    ),
     vscode.languages.registerCompletionItemProvider(
       "lst",
       completionItemProvider
@@ -26,13 +18,13 @@ export function activate(context: vscode.ExtensionContext) {
 const formattingEditProvider: vscode.DocumentFormattingEditProvider = {
   provideDocumentFormattingEdits: async (document, _, token) => {
     const edits = await getFormatRangeEdits(document);
-    return token.isCancellationRequested ? [] : (edits as TextEdit[]);
+    return token.isCancellationRequested ? [] : (edits as vscode.TextEdit[]);
   },
 };
 
 const getFormatRangeEdits = async (
   document: TextDocument
-): Promise<ReadonlyArray<TextEdit>> => {
+): Promise<ReadonlyArray<vscode.TextEdit>> => {
   let result = "";
   let indent = 0;
   for (const char of document.getText()) {
@@ -45,22 +37,22 @@ const getFormatRangeEdits = async (
     result += char;
   }
 
-  result = result.replace("\n", ""); // Remove newline at the beginning
-  result = result.replace(/ \n/g, "\n"); // Remove trailing spaces from each line
-  result = result.replace(/ *\]/g, "]"); // Remove spaces from before any ]
-  result = result.replace(/\n\s*\n/g, "\n"); // Remove empty lines
+  result = result
+    .replace("\n", "") // Remove newline at the beginning
+    .replace(/ \n/g, "\n") // Remove trailing spaces from each line
+    .replace(/ *\]/g, "]") // Remove spaces from before any ]
+    .replace(/\n\s*\n/g, "\n"); // Remove empty lines
 
   return [
-    new TextEdit(new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE), result),
+    new vscode.TextEdit(
+      new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE),
+      result
+    ),
   ];
 };
 
 const completionItemProvider: vscode.CompletionItemProvider = {
-  provideCompletionItems: async (
-    document: vscode.TextDocument,
-    position: Position,
-    token: vscode.CancellationToken
-  ): Promise<vscode.CompletionItem[]> => {
+  provideCompletionItems: async (document, position, token) => {
     const comps = await getCompletions(document, position);
     return token.isCancellationRequested ? [] : (comps as CompletionItem[]);
   },
@@ -70,11 +62,8 @@ const getCompletions = async (
   document: TextDocument,
   position: Position
 ): Promise<ReadonlyArray<CompletionItem>> => {
-  const textUntilPosition = document.getText(
-    new Range(new Position(0, 0), position)
-  );
-
-  const blocks = [...textUntilPosition.matchAll(/\[[\w\']*/g)];
+  const textToPos = document.getText(new Range(new Position(0, 0), position));
+  const blocks = [...textToPos.matchAll(/\[[\w\']*/g)];
 
   if (blocks.length === 0) {
     return [getCompletionFromRule(Object.entries(rules["Empty Page"])[0])];
